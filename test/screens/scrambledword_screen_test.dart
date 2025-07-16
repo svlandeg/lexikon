4e03292ea7a2388c7c163b250f0d5448392f2afc
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2025-07-16 12:33:58 +0200

    New exercise: scrambling words until correct (#58)
    * first implementation of scrambled words exercise
    
    * rename
    
    * first tests for scrambled word screen
    
    * cleanup
    
    * add 2 screenshots for the word scramble
    
    * add explanation and screenshots for unscrambling words
    
    * update img sizes

---
+++ test/screens/scrambledword_screen_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lexikon/scrambledword_screen.dart';
+import 'package:lexikon/vocabulary.dart';
+
+void main() {
+  group('ScrambledWordScreen', () {
+    testWidgets('shows source word and scrambled chips', (WidgetTester tester) async {
+      final vocab = Vocabulary(
+        id: '1',
+        name: 'TestVocab',
+        sourceLanguage: 'English',
+        targetLanguage: 'Spanish',
+        sourceReadingDirection: TextDirection.ltr,
+        targetReadingDirection: TextDirection.ltr,
+        entries: [Entry(source: 'cat', target: 'gato')],
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ScrambledWordScreen(vocabulary: vocab, count: 1),
+        ),
+      );
+
+      // Source word is shown
+      expect(find.text('English:'), findsOneWidget);
+      expect(find.text('cat'), findsOneWidget);
+
+      // Target language label is shown
+      expect(find.text('Spanish:'), findsOneWidget);
+
+      // Chips for each letter in 'gato' are shown (in any order)
+      for (final letter in 'tgao'.split('')) {
+        expect(find.text(letter), findsWidgets); // could be more than one if scrambled
+      }
+      // The 'Correct!' message should not be shown initially
+      expect(find.text('Correct!'), findsNothing);
+    });
+  });
+} 
