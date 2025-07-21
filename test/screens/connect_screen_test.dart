0d3118b94478fd9ce941a68def55c95f961fb47a
svlandeg <sofie.vanlandeghem@gmail.com>
2025-07-21 16:12:59 +0200

    connection test

--- test/screens/connect_screen_test.dart
+++ test/screens/connect_screen_test.dart
@@ -30,5 +30,46 @@ void main() {
       expect(find.byType(LinearProgressIndicator), findsOneWidget);
       expect(find.text('0/3'), findsOneWidget);
     });
+
+    testWidgets('can make a valid connection', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ConnectScreen(wordPairs: wordPairs),
+        ),
+      );
+      await tester.pumpAndSettle();
+
+      // Find the first source word and its correct target
+      final firstPair = wordPairs[0];
+      final sourceFinder = find.text(firstPair['source']!);
+      final targetFinder = find.text(firstPair['target']!);
+
+      // Before tapping, the source and target should be enabled (not greyed out)
+      final sourceBoxPreTap = tester.widget<Container>(find.ancestor(of: sourceFinder, matching: find.byType(Container)).first);
+      final targetBoxPreTap = tester.widget<Container>(find.ancestor(of: targetFinder, matching: find.byType(Container)).first);
+      final BoxDecoration? sourceDecorationPreTap = sourceBoxPreTap.decoration as BoxDecoration?;
+      final BoxDecoration? targetDecorationPreTap = targetBoxPreTap.decoration as BoxDecoration?;
+      expect(sourceDecorationPreTap?.color, equals(Colors.white));
+      expect(targetDecorationPreTap?.color, equals(Colors.white));
+
+      // Tap the source word
+      await tester.tap(sourceFinder);
+      await tester.pumpAndSettle();
+      // Tap the correct target word
+      await tester.tap(targetFinder);
+      await tester.pumpAndSettle();
+
+      // After connecting, the source and target should be disabled (greyed out)
+      final sourceBoxPostTap = tester.widget<Container>(find.ancestor(of: sourceFinder, matching: find.byType(Container)).first);
+      final targetBoxPostTap = tester.widget<Container>(find.ancestor(of: targetFinder, matching: find.byType(Container)).first);
+      final BoxDecoration? sourceDecorationPostTap = sourceBoxPostTap.decoration as BoxDecoration?;
+      final BoxDecoration? targetDecorationPostTap = targetBoxPostTap.decoration as BoxDecoration?;
+      expect(sourceDecorationPostTap?.color, equals(Colors.grey[300]));
+      expect(targetDecorationPostTap?.color, equals(Colors.grey[300]));
+
+      // Progress should update to 1/3
+      expect(find.text('1/3'), findsOneWidget);
+      expect(find.text('0/3'), findsNothing);
+    });
   });
 } 
